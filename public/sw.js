671acf86d1ab3389b539a4edca90ed182e90dc73
Mikolaj Zywczok <mikolaj.zywczok@gmail.com>
2020-03-20 12:59:28 +0100

    Cache for shell of the app

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,26 @@
 self.addEventListener('install', event => {
   console.log('[Service Worker] Installing Service Worker...', event);
+  event.waitUntil(
+    caches.open('static')
+      .then(cache => {
+        console.log('[Service Worker] Precaching App Shell...');
+        cache.addAll([
+          '/',
+          '/index.html',
+          '/src/js/app.js',
+          '/src/js/feed.js',
+          '/src/js/promise.js',
+          '/src/js/fetch.js',
+          '/src/js/material.min.js',
+          '/src/css/app.css',
+          '/src/css/feed.css',
+          '/src/images/main-image.jpg',
+          'https://fonts.googleapis.com/icon?family=Material+Icons',
+          'https://fonts.googleapis.com/css?family=Roboto:400,700',
+          'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
+        ]);
+      })
+  );
 });
 
 self.addEventListener('activate', event => {
@@ -8,5 +29,11 @@ self.addEventListener('activate', event => {
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) return response;
+        else return fetch(event.request);
+      })
+  );
 });
