5d5111af48a980a98aeb5049fe5a5aa4c208df73
Mikolaj Zywczok <mikolaj.zywczok@gmail.com>
2020-03-20 09:49:08 +0100

    add basic setup for service workers; add fetch api support for older browsers

---
+++ public/src/js/app.js
@@ -0,0 +1,20 @@
+var deferredPrompt;
+
+if (!window.Promise) {
+  window.Promise = Promise;
+}
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('/sw.js')
+    .then(() => {
+      console.log('Service worker registered!');
+    });
+}
+
+window.addEventListener('beforeinstallprompt', event => {
+  console.log('beforeinstallprompt fired');
+  event.preventDefault();
+  deferredPrompt = event;
+  return false;
+});
